ff11c78eb94ced1c562c77e999818d92aee92d6e
Erick Bacurau <erickbacurau1989@gmail.com>
2022-10-21 17:56:41 +0200

    feat: Implement printf over ITM
    edited syscalls.c to send printf streams to the ITM
    
    added more ignored files to gitignore

--- Scheduler/Scheduler/Src/main.c
+++ Scheduler/Scheduler/Src/main.c
@@ -17,13 +17,10 @@
  */
 
 #include <stdint.h>
-
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include <stdio.h>
 
 int main(void)
 {
-    /* Loop forever */
+  printf("This is a simple ITM test\n");
 	for(;;);
 }
